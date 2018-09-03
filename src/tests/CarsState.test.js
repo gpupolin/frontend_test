ad88a52f6b523967b42a70d8cad7d94ff81217e5
MicroPower - Guilherme Pupolin dos Santos <guilherme.pupolin@micropower.com.br>
2018-09-03 11:24:46 -0300

    Criação dos testes do estado do dominio de Carros para Aluguel

---
+++ src/tests/CarsState.test.js
@@ -0,0 +1,27 @@
+import reducer from "../store/cars/reducer";
+import AmadeusCarsRentalMock from "./mocks/AmadeusCarsRental";
+import { Record, List } from "immutable";
+
+const carstate = Record({
+  isFetching: false,
+  data: List()
+});
+
+const initialState = carstate({
+  isFetching: false,
+  data: List()
+});
+
+describe("mudanças do estado dos carros para aluguel", () => {
+  test("requisição dos dados", () => {
+    const state = reducer(initialState, { type: "cars.REQUESTED_DATA" });
+    expect(state.toJS()).toEqual({ isFetching: true, data: [] });
+  });
+  test("dados retornados", () => {
+    const state = reducer(initialState, {
+      type: "cars.FETCHED_DATA",
+      ...{ payload: AmadeusCarsRentalMock.results }
+    });
+    expect(state.data.toJS()).toEqual(AmadeusCarsRentalMock.results);
+  });
+});

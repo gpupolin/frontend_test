93709c1ec7d5e7246953a01ff0049a02b50b3c1b
Guilherme Pupolin <gpupolin@gmail.com>
2018-09-05 22:59:11 -0300

    Alterações no componente do VehicleSort

--- src/components/VehicleSort.jsx
+++ src/components/VehicleSort.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import styled, { css } from "styled-components";
+import Select from "react-select";
+
+// const Select = styled.select`
+//   background-color: #ffffff;
+//   border: solid 1px #e4e4e4;
+//   padding: 15px 10px;
+// `;
+
+const optionsItemByPages = [{ value: 10, label: "10" }, { value: 50, label: "50" }, { value: 100, label: "100" }];
+const optionsSort = [{ value: 10, label: "Menor preço" }, { value: 50, label: "Maior preço" }];
+const optionsCurrency = [{value: "USD", label: "USD"}]
 
 class VehicleSort extends Component {
   render() {
     return (
       <div>
-        <select>
-          <option>Itens por página:</option>
-        </select>
-        <select>
-          <option>Ordenar por:</option>
-        </select>
-        <select>
-          <option>Moeda:</option>
-        </select>
+        <Select options={optionsItemByPages} />
+        <Select options={optionsSort} />
+        <Select options={optionsCurrency} />
       </div>
     );
   }
